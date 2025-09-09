094e60406788d6dbe8c5335fa655fc46839a5d1b
Manju Venki <ddv.manju@gmail.com>
2025-09-09 16:00:27 +0530

    roboshop terraform

--- vm-module/data.tf
+++ vm-module/data.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "main" {
   name = "project-setup1"
 }
 
-data "azurerm_subnet" "main" {
+data "azurerm_subnet" "workstation" {
   name                 = "DevOps-AMI-vnet"
   virtual_network_name = "main"
   resource_group_name  = data.azurerm_resource_group.main.name
