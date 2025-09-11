6834f4e3dd0381e30236219f82050af26be59706
Manju Venki <ddv.manju@gmail.com>
2025-09-11 14:49:08 +0530

    roboshop terraform

--- vm-module/data.tf
+++ vm-module/data.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "main" {
 }
 
 data "azurerm_subnet" "main" {
-  name                 = "vnet-ukwest/snet-ukwest-1"
+  name                 = "vnet-ukwest"
   virtual_network_name = "main"
   resource_group_name  = data.azurerm_resource_group.main.name
 }
