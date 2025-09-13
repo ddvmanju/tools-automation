92aa3f151de662f0df7585e81001914c552de6c9
Manju Venki <ddv.manju@gmail.com>
2025-09-13 15:17:54 +0530

    roboshop terraform

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_network_security_group" "main" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "tostring(security_rule.var.port)"
+    destination_port_range     = var.port
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
